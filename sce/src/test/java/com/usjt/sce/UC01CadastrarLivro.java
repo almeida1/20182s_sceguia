1b4cd576eed4ef7863f0575712b0b0db0d3611eb
DV6-6170\Hp <engsoftware1@gmail.com>
2018-10-23 14:23:26 -0200

    Inclusao do data driven

--- sce/src/test/java/com/usjt/sce/UC01CadastrarLivro.java
+++ sce/src/test/java/com/usjt/sce/UC01CadastrarLivro.java
@@ -2,6 +2,8 @@ package com.usjt.sce;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 
 import com.usjt.sce.model.DAOFactory;
@@ -9,6 +11,13 @@ import com.usjt.sce.model.ILivroDAO;
 import com.usjt.sce.model.Livro;
 
 public class UC01CadastrarLivro {
+	@AfterClass
+	public static void reverter() {
+		Livro umLivro = ObtemLivro.comDadosValidos();
+		DAOFactory mySQLFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+		ILivroDAO livroDAO = mySQLFactory.getLivroDAO();
+		livroDAO.exclui(umLivro.getIsbn());
+	}
 
 	@Test
 	public void CT01CadastrarLivroComDadosValidos() {
@@ -24,6 +33,7 @@ public class UC01CadastrarLivro {
 			fail("nao deve falhar");
 		}
 	}
+
 	@Test
 	public void CT02CadastrarLivroComISBNBranco() {
 		try {
@@ -35,20 +45,39 @@ public class UC01CadastrarLivro {
 			umLivro.setAutor("Pressman");
 		} catch (RuntimeException e) {
 			// verificacao
-			assertEquals("ISBN invalido",e.getMessage());
+			assertEquals("ISBN invalido", e.getMessage());
 		}
 	}
+
 	@Test
-	public void CT03CadastrarLivro_com_sucesso(){
-		//cenario
+	public void CT03CadastrarLivro_com_sucesso() {
+		// cenario
 		Livro umLivro = ObtemLivro.comDadosValidos();
 		DAOFactory mySQLFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
 		ILivroDAO livroDAO = mySQLFactory.getLivroDAO();
-		//acao
+		// acao
 		int codigoRetorno = livroDAO.adiciona(umLivro);
-		//verificacao
-		assertEquals(1,codigoRetorno);
-        livroDAO.exclui(umLivro.getIsbn());
+		// verificacao
+		assertEquals(1, codigoRetorno);
+		
 	}
-	
+
+	@Test
+	public void CT04CadastrarLivro_dados_invalidos_isbn_ja_cadastrado() {
+		// cenario
+		Livro umLivro = ObtemLivro.comDadosValidos();
+		DAOFactory mySQLFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
+		ILivroDAO livroDAO = mySQLFactory.getLivroDAO();
+		// acao
+		try {
+			livroDAO.adiciona(umLivro);
+			livroDAO.adiciona(umLivro);
+			fail("deveria falhar - a aplicação não deve permtir a inclusão de um isbn repetido");
+		} catch (Exception e) {
+			// verificacao
+			System.out.println(e.getMessage());
+			assertEquals("com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException: Duplicate entry '121212' for key 'PRIMARY'", e.getMessage());
+		}
+	}
+
 }
